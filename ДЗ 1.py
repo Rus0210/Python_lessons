aba87772aa55f812768fe9113b278ac474959251
Роберт Шагалиев <robertsagaliev@iMac-Robert.local>
2024-06-02 13:24:29 +0500

    Initial commit

---
+++ ДЗ 1.py
@@ -0,0 +1,8 @@
+i = 0
+for a in ("АКРУ"):
+    for b in ("АКРУ"):
+        for c in ("АКРУ"):
+            for d in ("АКРУ"):
+                for e in ("АКРУ"):
+                    i += 1
+                    print(i, a,b,c,d,e)
